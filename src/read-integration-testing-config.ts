ffc59e3b0dbf5ca2c827d651b65c69a7fe9aa570
Marius B. Kotsbak <marius.kotsbak@webstep.no>
2022-01-19 10:46:13 +0100

    Add read-integration-testing-config #minor

---
+++ src/read-integration-testing-config.ts
@@ -0,0 +1,90 @@
+import * as core from '@actions/core'
+import * as fs from 'fs'
+
+/**
+ * TODO: consider extracting common parts in this and "read-terraform-config.ts"
+ */
+
+type ActionConfig = {
+  workloadIdentityProjectId?: string
+  workloadIdentityProjectNumber?: string
+  environment?: string
+}
+
+export type IntTestingConfig = {
+  environment: string
+  workloadIdentityProjectId?: string
+  workloadIdentityProjectNumber?: string
+}
+
+export async function readIntTestingConfig(
+  projectDir: string,
+  configFileName: string
+): Promise<IntTestingConfig | null> {
+  let workloadIdentityProjectId: string | undefined = undefined
+  let workloadIdentityProjectNumber: string | undefined = undefined
+  let environment = 'SHARED'
+
+  const configPath = `${projectDir}/${configFileName}`
+  if (fs.existsSync(configPath)) {
+    const file = fs.readFileSync(configPath, 'utf-8')
+    const config = JSON.parse(file) as ActionConfig
+    if (config.workloadIdentityProjectId) {
+      workloadIdentityProjectId = config.workloadIdentityProjectId
+    }
+    if (config.workloadIdentityProjectNumber) {
+      workloadIdentityProjectNumber = config.workloadIdentityProjectNumber
+    }
+    if (config.environment) {
+      environment = config.environment
+    }
+  } else {
+    core.info(`No ${configFileName} found. Unable to run integration tests.`)
+    return null
+  }
+
+  return {
+    environment,
+    workloadIdentityProjectId,
+    workloadIdentityProjectNumber,
+  }
+}
+
+async function run(): Promise<void> {
+  try {
+    const projectRoot = core.getInput('cwd')
+
+    const res = await readIntTestingConfig(projectRoot, 'inttest.config.json')
+    if (!res) {
+      core.setFailed('No integration testing config file found')
+    } else {
+      const {
+        environment,
+        workloadIdentityProjectId,
+        workloadIdentityProjectNumber,
+      } = res
+
+      if (environment) {
+        core.setOutput('environment', environment)
+      }
+
+      if (workloadIdentityProjectId) {
+        core.setOutput(
+          'workload_identity_project_id',
+          workloadIdentityProjectId
+        )
+      }
+
+      if (workloadIdentityProjectNumber) {
+        core.setOutput(
+          'workload_identity_project_number',
+          workloadIdentityProjectNumber
+        )
+      }
+    }
+  } catch (error: any) {
+    core.setFailed(error)
+  }
+}
+
+void run()
