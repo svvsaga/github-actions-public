a4db43349d1dc9c5661684f2c40c43a52f22103a
Geir Sagberg <geir.sagberg@gmail.com>
2022-01-31 19:11:06 +0100

    Add replace-tfvars-with-secrets #minor

--- src/implementations/replace-tfvars-with-secrets.ts
+++ src/implementations/replace-tfvars-with-secrets.ts
@@ -1,3 +1,39 @@
-export async function replaceTfvarsWithSecrets(): Promise<void> {}
+import { readFileSync, writeFileSync } from 'fs'
+import { resolve } from 'path'
 
+const regex = /\$(\w+)/
+
+export async function replaceTfvarsWithSecrets({
+  secrets,
+  terraformDir,
+  secretsFile,
+}: {
+  secrets: Record<string, string>
+  terraformDir: string
+  secretsFile: string
+}): Promise<void> {
+  const tfvars = readFileSync(resolve(terraformDir, secretsFile), 'utf8')
+  const replaced = replaceTfvars(tfvars, secrets)
+  writeFileSync(resolve(terraformDir, secretsFile), replaced)
+}
 export default replaceTfvarsWithSecrets
+
+export function replaceTfvars(
+  tfvars: string,
+  secrets: Record<string, string>
+): string {
+  return tfvars
+    .split('\n')
+    .map((line) => {
+      const result = regex.exec(line)
+      if (result != null) {
+        const [, key] = result
+        const value = secrets[key]
+        if (value != null) {
+          line = line.replace(regex, value)
+        }
+      }
+      return line
+    })
+    .join('\n')
+}
